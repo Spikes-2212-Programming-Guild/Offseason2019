a3a9b76ef22af66fb55643d750e118049c25811b
YuvalLevy1 <Yuval124levy1@gmail.com>
2019-09-27 12:48:26 +0300

    created a separated class for the gripper

---
+++ src/main/java/frc/robot/subsystem/Gripper.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.command.genericsubsystem.GenericSubsystem;
+import com.spikes2212.dashboard.ConstantHandler;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import frc.robot.RobotMap;
+
+import java.util.function.Supplier;
+
+public class Gripper extends GenericSubsystem {
+
+    public static Supplier<Double> IN_SPEED = ConstantHandler.addConstantDouble("gripper in speed", -0.7);
+    public static Supplier<Double> OUT_SPEED = ConstantHandler.addConstantDouble("gripper out speed", -0.7);
+
+    SpeedControllerGroup gripperSC = new SpeedControllerGroup
+            (new WPI_TalonSRX(RobotMap.PWM.GRIPPER_RIGHT),
+                    new WPI_TalonSRX(RobotMap.PWM.GRIPPER_LEFT));
+
+    DigitalInput limit = new DigitalInput(RobotMap.DIO.limitSwitch);
+
+    @Override
+    public void apply(double v) {
+        gripperSC.set(v);
+    }
+
+    @Override
+    public boolean canMove(double v) {
+        if(v > 0) {
+            return true;
+        }
+        return !limit.get();
+    }
+
+    @Override
+    public void stop() {
+        gripperSC.stopMotor();
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
