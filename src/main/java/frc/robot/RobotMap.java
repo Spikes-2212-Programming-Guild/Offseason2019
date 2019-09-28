f19ac7b91e8ae9ec3687bb78314acd36905f473c
Matangn13 <matangn13@gmail.com>
2019-09-28 16:04:28 +0300

    corrected ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -11,12 +11,12 @@ package frc.robot;
 public class RobotMap {
     public static interface CAN {
         int LIFT_MASTER = 0;
-        int LIFT_SLAVE_0 = 1;
-        int LIFT_SLAVE_1 = 2;
+        int LIFT_SLAVE_0 = 2;
+        int LIFT_SLAVE_1 = 3;
     }
 
     public static interface DIO {
-        int LIFT_LIMIT_BOTTOM = 0;
-        int LIFT_LIMIT_TOP = 1;
+        int LIFT_LIMIT_BOTTOM = 8;
+        int LIFT_LIMIT_TOP = 9;
     }
 }
