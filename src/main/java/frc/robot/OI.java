7c468b8184d4e25452a5462b6352379b34f8a14d
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-10-03 20:20:52 +0300

    added arm to OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,6 +10,8 @@ package frc.robot;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import com.spikes2212.utils.XboXUID;
 import edu.wpi.first.wpilibj.buttons.Button;
+import frc.robot.commands.arm.ToBack;
+import frc.robot.commands.arm.ToFront;
 import frc.robot.subsystems.Gripper;
 
 import frc.robot.commands.latch.LatchClose;
@@ -19,19 +21,24 @@ import edu.wpi.first.wpilibj.Joystick;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI {
+public class OI /* Gevald! */ {
     private XboXUID xbox = new XboXUID(2);
     private Button openLatch=xbox.getLBButton();
     private Button closeLatch=xbox.getLTButton();
     private Button gripperIn= xbox.getRBButton();
     private Button gripperOut= xbox.getRTButton();
+    private Button moveArmFront=xbox.getUpButton();
+    private Button moveArmBack=xbox.getDownButton();
     private static Joystick leftJoystick=new Joystick(0);
     private static Joystick rightJoystick=new Joystick(1);
+
     public OI() {
         gripperIn.whileHeld(new MoveGenericSubsystem(Robot.gripper, Gripper.IN_SPEED));
         gripperOut.whileHeld(new MoveGenericSubsystem(Robot.gripper, Gripper.OUT_SPEED));
         openLatch.whenPressed(new LatchOpen());
         closeLatch.whenPressed(new LatchClose());
+        moveArmFront.whenPressed(new ToFront());
+        moveArmBack.whenPressed(new ToBack());
     }
 
 
