ba8abf84bcb9a9c0b7ec5b9a26b71091f4c47436
Spikes2212 <spikes.no2212@gmail.com>
2019-10-05 10:42:07 +0300

    changed GYRO to ORIENT

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -27,15 +27,15 @@ public class Drivetrain extends TankDrivetrain {
 
     public static final PIDSettings FORWARD_PID_SETTINGS = new PIDSettings(FORWARD_KP, FORWARD_KI, FORWARD_KD, FORWARD_TOLERANCE, FORWARD_WAIT_TIME);
 
-    public static final Namespace GYRO_PID = NAMESPACE.addChild("Forward PID");
+    public static final Namespace ORIENT_PID = NAMESPACE.addChild("Forward PID");
 
-    public static final Supplier<Double> GYRO_KP = GYRO_PID.addConstantDouble("KP", 0.5);
-    public static final Supplier<Double> GYRO_KI = GYRO_PID.addConstantDouble("KI", 0.0);
-    public static final Supplier<Double> GYRO_KD = GYRO_PID.addConstantDouble("KD", 0.0);
-    public static final Supplier<Double> GYRO_WAIT_TIME = GYRO_PID.addConstantDouble("Wait Time", 0.1);
-    public static final Supplier<Double> GYRO_TOLERANCE = GYRO_PID.addConstantDouble("Tolerance", 1.0);
+    public static final Supplier<Double> ORIENT_KP = ORIENT_PID.addConstantDouble("KP", 0.5);
+    public static final Supplier<Double> ORIENT_KI = ORIENT_PID.addConstantDouble("KI", 0.0);
+    public static final Supplier<Double> ORIENT_KD = ORIENT_PID.addConstantDouble("KD", 0.0);
+    public static final Supplier<Double> ORIENT_WAIT_TIME = ORIENT_PID.addConstantDouble("Wait Time", 0.1);
+    public static final Supplier<Double> ORIENT_TOLERANCE = ORIENT_PID.addConstantDouble("Tolerance", 1.0);
 
-    public static final PIDSettings GYRO_PID_SETTINGS = new PIDSettings(GYRO_KP, GYRO_KI, GYRO_KD, GYRO_TOLERANCE, GYRO_WAIT_TIME);
+    public static final PIDSettings ORIENT_PID_SETTINGS = new PIDSettings(ORIENT_KP, ORIENT_KI, ORIENT_KD, ORIENT_TOLERANCE, ORIENT_WAIT_TIME);
 
 
     public Drivetrain(SpeedController left, SpeedController right, Encoder leftEncoder, Encoder rightEncoder, GyroBase gyro) {
