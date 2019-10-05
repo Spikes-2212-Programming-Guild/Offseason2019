bfef8a4f24cbaddc4328296feea347bf41453217
Spikes2212 <spikes.no2212@gmail.com>
2019-10-05 13:15:39 +0300

    added SetLiftState to LowerLift

--- src/main/java/frc/robot/commands/lift/LowerLift.java
+++ src/main/java/frc/robot/commands/lift/LowerLift.java
@@ -1,11 +1,15 @@
 package frc.robot.commands.lift;
 
+import com.spikes2212.utils.RunnableCommand;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
+import frc.robot.subsystems.Lift;
 
-public class LowerLift extends Command {
+public class LowerLift extends CommandGroup {
     public LowerLift() {
         requires(Robot.lift);
+        addSequential(new SetLiftState(Lift.LiftState.DOWN));
     }
 
     @Override
