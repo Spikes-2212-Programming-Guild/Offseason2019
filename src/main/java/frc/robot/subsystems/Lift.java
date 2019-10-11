ee70ca74621c40845c7347221288d72cdb0cd0e6
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-10-11 10:25:30 +0300

    fixed height configuration for encoder

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -38,6 +38,20 @@ public class Lift extends GenericSubsystem {
         public abstract boolean canMove(double speed);
     }
 
+    public static class TranslateLiftHeight implements Supplier<Double> {
+
+        private Supplier<Double> wrapped;
+
+        public TranslateLiftHeight(Supplier<Double> wrapped) {
+            this.wrapped = wrapped;
+        }
+
+        @Override
+        public Double get() {
+            return wrapped.get() - INITIAL_HEIGHT.get();
+        }
+    }
+
     public static final Namespace NAMESPACE = ConstantHandler.addNamespace("Lift");
     public static final Namespace UP_PID_NAMESPACE = NAMESPACE.addChild("Up PID");
     public static final Namespace DOWN_PID_NAMESPACE = NAMESPACE.addChild("Down PID");
@@ -54,7 +68,7 @@ public class Lift extends GenericSubsystem {
 
     public static final Supplier<Double> SWITCH_POINT = NAMESPACE.addConstantDouble("PID Switch Point", 40);
     public static final Supplier<Double> SWITCH_HEIGHT = NAMESPACE.addConstantDouble("Switch Point Height", 0.708);
-    public static final Supplier<Double> INITIAL_HEIGHT = NAMESPACE.addConstantDouble("Initial Latch Height", 0.395);
+    public static final Supplier<Double> INITIAL_HEIGHT = NAMESPACE.addConstantDouble("Initial Height", 0.395);
 
     public static final Supplier<Double> KP_UP = UP_PID_NAMESPACE.addConstantDouble("KP", 1);
     public static final Supplier<Double> KI_UP = UP_PID_NAMESPACE.addConstantDouble("KI", 1);
@@ -68,11 +82,12 @@ public class Lift extends GenericSubsystem {
     public static final Supplier<Double> TOLERANCE_DOWN = DOWN_PID_NAMESPACE.addConstantDouble("Tolerance", 1);
     public static final Supplier<Double> WAIT_TIME_DOWN = DOWN_PID_NAMESPACE.addConstantDouble("Wait Time", 1);
 
-    public static final Supplier<Double> TOP_SETPOINT = SETPOINTS.addConstantDouble("top", 1.811);
-    public static final Supplier<Double> LEVEL_2_SETPOINT = SETPOINTS.addConstantDouble("level 2", 0.926);
-    public static final Supplier<Double> CARGO_SHIP_ABOVE_SETPOINT = SETPOINTS.addConstantDouble("cargo ship above", 0.572);
-    public static final Supplier<Double> CARGO_SHIP_BELOW_SETPOINT = SETPOINTS.addConstantDouble("cargo ship below", 1.28);
+    public static final Supplier<Double> TOP_SETPOINT = new TranslateLiftHeight(SETPOINTS.addConstantDouble("top", 1.9));
+    public static final Supplier<Double> LEVEL_2_SETPOINT = new TranslateLiftHeight(SETPOINTS.addConstantDouble("level 2", 0.926));
+    public static final Supplier<Double> CARGO_SHIP_ABOVE_SETPOINT = new TranslateLiftHeight(SETPOINTS.addConstantDouble("cargo ship above", 0.572));
+    public static final Supplier<Double> CARGO_SHIP_BELOW_SETPOINT = new TranslateLiftHeight(SETPOINTS.addConstantDouble("cargo ship below", 1.28));
 
+    public static final double LIFT_DISTANCE_PER_PULSE = (1.9-0.395) / 80*4096;
     public static final PIDSettings UP_PID_SETTINGS =
             new PIDSettings(KP_UP, KI_UP, KD_UP, TOLERANCE_UP, WAIT_TIME_UP);
 
@@ -148,7 +163,5 @@ public class Lift extends GenericSubsystem {
         setDefaultCommand(new LowerLift());
     }
 
-    public static double encoderValueFromHeight(double height) {
-        return (height - INITIAL_HEIGHT.get()) * SWITCH_POINT.get() / SWITCH_HEIGHT.get();
-    }
+
 }
